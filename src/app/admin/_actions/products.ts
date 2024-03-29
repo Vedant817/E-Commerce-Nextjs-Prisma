6b9d7efc2532be1eb0a8f139c358ad4f378bca36
Vedant Mahajan <vedantmahajan271@gmail.com>
2024-03-29 13:38:54 +0530

    Fixing Update Schema

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -100,7 +100,6 @@ export async function updateProduct(
     await db.product.update({
         where: { id },
         data: {
-            isAvailableForPurchase: false,
             name: data.name,
             description: data.description,
             priceInCents: data.priceInCents,
